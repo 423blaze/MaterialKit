990d928006694035c91e0c0e5a255af32d7f33d8
423blaze <cookjames0423@gmail.com>
2019-09-03 19:01:27 +0300

    bug-fixing JS file

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -1,6 +1,6 @@
- // 
+ //
  // =========================================================
- // * Material Kit - v2.0.5
+ // * Material Kit - v2.0.6
  // =========================================================
  //
  // * Product Page: https://www.creative-tim.com/product/material-kit
@@ -12,8 +12,6 @@
  //
  // * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-
-<!DOCTYPE html>
 var big_image;
 
 $(document).ready(function() {
